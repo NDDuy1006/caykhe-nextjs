62ab7759355c639f85763942ab33be59a8ed5246
Duy ND <duynd1006@gmail.com>
2025-09-25 22:30:10 +0100

    fix button

--- src/app/components/CtaButton/CtaButton.tsx
+++ src/app/components/CtaButton/CtaButton.tsx
@@ -1,16 +1,29 @@
+"use client"
+
 import { twMerge } from "tailwind-merge";
 
 interface IProps {
+  sectionId: string;
   primary?: boolean;
   children: React.ReactNode
 }
 
-const CtaButton = ({ primary, children }: IProps) => {
+const CtaButton = ({ sectionId, primary, children }: IProps) => {
+  const handleScroll = (id: string) => {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <a className={twMerge(
-      `block px-4 py-2 cursor-pointer transition-all duration-400 ease-in-out uppercase font-bold,
-      ${primary ? 'bg-[#C8AC77] hover:opacity-75 hover:scale-105' : 'border-[1px] border-solid border-[#C8AC77] hover:bg-[#C8AC77]'}`
-    )}>
+    <a
+      className={twMerge(
+        `block px-4 py-2 cursor-pointer transition-all duration-400 ease-in-out uppercase font-bold,
+        ${primary ? 'bg-[#C8AC77] hover:opacity-75 hover:scale-105' : 'border-[1px] border-solid border-[#C8AC77] hover:bg-[#C8AC77]'}`
+      )}
+      onClick={() => handleScroll(sectionId)}
+    >
       {children}
     </a>
   )
