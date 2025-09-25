7d82ef48d83fff8646a7fdff2f4eac40f6631e8c
Duy ND <duynd1006@gmail.com>
2025-09-25 18:23:58 +0100

    fix spacing

--- src/app/components/Menu/MenuColumn.tsx
+++ src/app/components/Menu/MenuColumn.tsx
@@ -2,7 +2,7 @@
 
 const MenuColumn = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="px-5 md:px-0">
+    <div className="md:px-0">
       {children}
     </div>
   )
